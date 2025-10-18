af68491dd794019211f1ae6673a4f9f31f0df9bd
23001549@hus.edu.vn <23001549@hus.edu.vn>
2025-10-18 18:38:31 +0700

    Add header and login page

---
+++ front-end/src/components/Container.tsx
@@ -0,0 +1,5 @@
+import type { ReactNode } from 'react';
+
+export default function Container({ children }: { children: ReactNode }) {
+  return <div className="container mx-auto px-4">{children}</div>;
+}
